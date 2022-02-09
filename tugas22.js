2ff650c532722ee561a66b72c1a49213ba2e324a
Firman Ismail <akayasha99@gmail.com>
2022-02-10 00:20:40 +0700

    tugas 22 js

---
+++ tugas22.js
@@ -0,0 +1,10 @@
+var string = "Saya Ingin Belajar Bersama";
+var result = string.split(' ');
+function panggilffor() {
+  var data = result
+  data.forEach((item,Index) => {
+    console.log("item : ",item,"index ke ",Index);
+  });
+
+}
+panggilffor()
